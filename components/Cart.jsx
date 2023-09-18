bdcdb7a5d1f1618130110141c99c9a4e83c17d15
GitJuanJ-code <topjuanj@gmail.com>
2023-09-18 14:18:25 -0500

    Ultimos Cambios

---
+++ components/Cart.jsx
@@ -0,0 +1,11 @@
+import React, { useRef } from 'react';
+import Link from 'next/link';
+import { AiOutlineMinus, AiOut}
+
+const Cart = () => {
+  return (
+    <div>Cart</div>
+  )
+}
+
+export default Cart
